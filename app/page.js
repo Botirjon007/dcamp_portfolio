b9774a7a00effa1993fef80f776f759d9947abbf
Botirjon007 <batyrzhanulmasov@gmail.com>
2024-02-27 04:02:12 +0500

    updated lists of homw with underline hover

--- app/page.js
+++ app/page.js
@@ -2,68 +2,122 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="container">
-      <div className="content">
-        <div className="mt-5 d-flex flex-column  align-items-center">
-          <h1>
-            <strong>Components List</strong>
-          </h1>
-          <br />
-          <a className="text" href="/homepage">
-            <p>Homepage</p>
-          </a>
-          <a className="text" href="/product">
-            <p>Product Details</p>
-          </a>
-          <a className="text" href="/cart">
-            <p>Cart</p>
-          </a>
-          <a className="text" href="/login">
-            <p>Login</p>
-          </a>
-          <a className="text" href="/register">
-            <p>Register</p>
-          </a>
-          <a className="text" href="/shipping">
-            <p>Shipping</p>
-          </a>
-          <a className="text" href="/profile">
-            <p>Profile</p>
-          </a>
-          <a className="text" href="address/new">
-            <p>Add new address</p>
-          </a>
-          <a className="text" href="/profile/update">
-            <p>Update Profile</p>
-          </a>
-          <a className="text" href="/update/password">
-            <p>Update Password</p>
-          </a>
-          <a className="text" href="/user/orders">
-            <p>User Order</p>
-          </a>
-          <a className="text" href="/product/new">
-            <p>New Product</p>
-          </a>
-          <a className="text" href="/product/all">
-            <p>All Products Admin</p>
-          </a>
-          <a className="text" href="/upload/images">
-            <p>Upload images</p>
-          </a>
-          <a className="text" href="/orders/admin">
-            <p>Orders Admin</p>
-          </a>
-          <a className="text" href="/order/update">
-            <p>Update Order</p>
-          </a>
-          <a className="text" href="/users/admin">
-            <p>Users Admin</p>
-          </a>
-          <a className="text" href="/update/users/admin">
-            <p>Update User Admin</p>
-          </a>
-        </div>
+    <div>
+      <div class="text-center">
+        <h1 class="text-3xl my-10 font-bold">Components List</h1>
+        <a
+          href="/products"
+          className="text-xl block text-black no-underline hover:underline"
+        >
+          <p class="text-xl">Hompage</p>
+        </a>
+        <a
+          href="/product/details"
+          className="text-xl block text-black no-underline hover:underline"
+        >
+          <p class="text-xl">Product Details</p>
+        </a>
+        <a
+          href="/cart"
+          className="text-xl block text-black no-underline hover:underline"
+        >
+          <p
+            class="text-xl"
+            className="text-xl block text-black no-underline hover:underline"
+          >
+            Cart
+          </p>
+        </a>
+        <a
+          href="/login"
+          className="text-xl block text-black no-underline hover:underline"
+        >
+          <p class="text-xl">Login</p>
+        </a>
+        <a
+          href="/register"
+          className="text-xl block text-black no-underline hover:underline"
+        >
+          <p class="text-xl">Register</p>
+        </a>
+        <a
+          href="/shipping"
+          className="text-xl block text-black no-underline hover:underline"
+        >
+          <p class="text-xl">Shipping</p>
+        </a>
+        <a
+          href="/profile"
+          className="text-xl block text-black no-underline hover:underline"
+        >
+          <p class="text-xl">Profile</p>
+        </a>
+        <a
+          href="/address/new"
+          className="text-xl block text-black no-underline hover:underline"
+        >
+          <p class="text-xl">Add new address</p>
+        </a>
+        <a
+          href="/profile/update"
+          className="text-xl block text-black no-underline hover:underline"
+        >
+          <p class="text-xl">Update Profile</p>
+        </a>
+        <a
+          href="/password/update"
+          className="text-xl block text-black no-underline hover:underline"
+        >
+          <p class="text-xl">Update Password</p>
+        </a>
+        <a
+          href="/me/orders"
+          className="text-xl block text-black no-underline hover:underline"
+        >
+          <p class="text-xl">User Order</p>
+        </a>
+        <a
+          href="/product/new"
+          className="text-xl block text-black no-underline hover:underline"
+        >
+          <p class="text-xl">New Product</p>
+        </a>
+        <a
+          href="/admin/products"
+          className="text-xl block text-black no-underline hover:underline"
+        >
+          <p class="text-xl">All Products Admin</p>
+        </a>
+        <a
+          href="/admin/upload_images"
+          className="text-xl block text-black no-underline hover:underline"
+        >
+          <p class="text-xl">Upload Images</p>
+        </a>
+        <a
+          href="/admin/orders"
+          className="text-xl block text-black no-underline hover:underline"
+        >
+          <p class="text-xl">Orders Admin</p>
+        </a>
+        <a
+          href="/admin/order/update"
+          className="text-xl block text-black no-underline hover:underline"
+        >
+          <p class="text-xl">Update Order</p>
+        </a>
+        <a
+          href="/admin/users"
+          className="text-xl block text-black no-underline hover:underline"
+        >
+          <p class="text-xl">Users Admin</p>
+        </a>
+        <a
+          href="/admin/user/update"
+          className="text-xl block text-black no-underline hover:underline"
+        >
+          <p class="text-xl">Update User Admin</p>
+        </a>
       </div>
     </div>
   );
